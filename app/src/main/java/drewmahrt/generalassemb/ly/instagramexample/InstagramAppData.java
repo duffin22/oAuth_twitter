cd23cb4d6c6707b9f3ace37c1aaf99a17b01146c
Mauve3 <mauvemoonman@gmail.com>
2016-08-10 14:45:22 -0700

    something

---
+++ app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/InstagramAppData.java
@@ -0,0 +1,10 @@
+package drewmahrt.generalassemb.ly.instagramexample;
+
+/**
+ * Created by drewmahrt on 4/21/16.
+ */
+public class InstagramAppData {
+    public static final String CLIENT_ID = "e7aa9d375b964348be3ccf2fe6e5fe19";
+    public static final String CLIENT_SECRET = "c6de5dd82a3f4deb9ded09faa22187fa";
+    public static final String CALLBACK_URL = "http://www.hopshares.com";
+}
